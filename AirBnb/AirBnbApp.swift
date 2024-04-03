b4c2253c4c2685ae85eb06ea754edd6b1020f570
vhenriquebm <vhenriquebm@gmail.com>
2024-04-03 18:31:47 -0300

    Initial Commit

---
+++ AirBnb/AirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbApp.swift
+//  AirBnb
+//
+//  Created by Vitor Henrique Barreiro Marinho on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
