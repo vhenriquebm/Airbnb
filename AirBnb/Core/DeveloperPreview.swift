f8c4d13430275a364b7403c6a9b1fc60fee5d00b
vhenriquebm <vhenriquebm@gmail.com>
2024-06-18 20:59:26 -0300

    Model created for developer preview

---
+++ AirBnb/Core/DeveloperPreview.swift
@@ -0,0 +1,18 @@
+//
+//  DeveloperPreview.swift
+//  AirBnb
+//
+//  Created by Vitor Henrique Barreiro Marinho on 18/06/24.
+//
+
+import Foundation
+
+class DeveloperPreview {
+    var listings: [Listing] = [
+    
+    
+    ]
+    
+    
+    
+}
---
+++ AirBnb/Core/Listings/Model/Listing.swift
@@ -0,0 +1,110 @@
+//
+//  Listing.swift
+//  AirBnb
+//
+//  Created by Vitor Henrique Barreiro Marinho on 18/06/24.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable {
+    let id: String
+    let ownerUid: String
+    let ownerName: String
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let city: String
+    let state: String
+    let title: String
+    var rating: Double
+    var features: [ListingFeatures]
+    var amenities: [ListingAmenities]
+    var type: [ListingType]
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable {
+    case selfCheckIn
+    case superhost
+    
+    var title: String {
+        switch self {
+        case .selfCheckIn: return "Self check-in"
+        case .superhost: return "Superhost"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .selfCheckIn: return "door.left.hand.open"
+        case .superhost: return "medal"
+        }
+    }
+    
+    var id: Int {return self.rawValue}
+}
+
+
+enum ListingAmenities: Int, Codable, Identifiable, Hashable {
+    case pool
+    case kitchen
+    case wifi
+    case laundry
+    case tv
+    case alarmSystem
+    case office
+    case balcony
+    
+    var title: String {
+        switch self {
+        case .pool: return "Pool"
+        case .kitchen: return "Kitchen"
+        case .wifi: return "Wifi"
+        case .laundry: return "Laundry"
+        case .tv: return "TV"
+        case .alarmSystem: return "Alarm System"
+        case .office: return "Office"
+        case .balcony: return "Balcony"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .pool: return "figure.pool.swim"
+        case .kitchen: return "fork.knife"
+        case .wifi: return "wifi"
+        case .laundry: return "washer"
+        case .tv: return "tv"
+        case .alarmSystem: return "checkkerboard.shield"
+        case .office: return "peneil.and.ruler.fill"
+        case .balcony: return "building"
+        }
+    }
+    
+    var id: Int {return self.rawValue}
+}
+
+enum ListingType: Int, Codable, Identifiable, Hashable {
+    case apartment
+    case house
+    case townHouse
+    case ville
+    
+    var description: String {
+        
+        switch self {
+        case .apartment: return "Apartment"
+        case .house: return "House"
+        case .townHouse: return "Town House"
+        case .ville: return "Ville"
+        }
+    }
+    
+    var id: Int {return self.rawValue}
+}
